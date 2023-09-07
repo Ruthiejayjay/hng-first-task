f8f44e49134bbaa04f4be5b22f8a48fdbe413234
Ruth James <jamesruth449@gmail.com>
2023-09-07 20:39:47 +0100

    fixed date format

--- app/Http/Controllers/FirstTaskController.php
+++ app/Http/Controllers/FirstTaskController.php
@@ -17,11 +17,13 @@ class FirstTaskController extends Controller
             ]);
             $slack_name = $request->query('slack_name');
             $track = $request->query('track');
+            $date = Carbon::now();
+            $date->toTimeString();
 
             return response()->json([
                 'slack_name' => $slack_name,
                 'current_day' => now()->format('l'),
-                'utc_time' => Carbon::now(),
+                'utc_time' => $date->toIso8601ZuluString(),
                 'track' => $track,
                 'github_file_url' => 'https://github.com/Ruthiejayjay/hng-first-task/blob/main/app/Http/Controllers/FirstTaskController.php',
                 'github_repo_url' => 'https://github.com/Ruthiejayjay/hng-first-task',
